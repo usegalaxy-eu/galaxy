0d3e613c7f31aabe897ba0992a5f3cd26a5613f3
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2019-01-30 15:44:06 +0100

    Merge gcc into iframe webhook

--- config/plugins/webhooks/iframe/__init__.py
+++ config/plugins/webhooks/iframe/__init__.py
@@ -1,5 +1,9 @@
 import random
 
+gcc_announcement = {
+    'target': 'https://goo.gl/forms/lcPoMt4iZ8hwcdM12?refresh=1m&orgId=1&from=now-3h&to=now&panelId=38',
+    'announce': 'Vote for your favorite trainings <em>HERE & NOW</em>!'
+}
 
 def main(trans, webhook, params):
     return random.choice([
@@ -17,5 +21,23 @@ def main(trans, webhook, params):
             'src': 'https://usegalaxy-eu.github.io/galaxy/events.html',
             'height': 1000,
             'title': 'Upcoming Events',
+        },
+        {
+            'src': 'https://galaxyproject.org/events/gcc2019/',
+            'height': 1000,
+            'title': 'Galaxy Community Conference 2019 - Freiburg, Germany',
+            'announcement': gcc_announcement
+        },
+        {
+            'src': 'https://goo.gl/forms/lcPoMt4iZ8hwcdM12?refresh=1m&orgId=1&from=now-3h&to=now&panelId=38',
+            'height': 1000,
+            'title': 'Galaxy Community Conference 2019 - Vote on Trainings',
+            'announcement': gcc_announcement
+        },
+        {
+            'src': 'https://galaxyproject.org/events/gcc2019/training/#training-at-gcc2019',
+            'height': 1000,
+            'title': 'Galaxy Community Conference 2019 - Training Topics',
+            'announcement': gcc_announcement
         }
     ])
