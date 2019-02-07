6fe4df6d6051589763af838499c6d35136dca6e9
Sebastian Kapust <sebastian@kapust.org>
2019-02-07 10:52:51 +0100

    Add infinite scroll, collapsible citations and info modal

--- client/galaxy/scripts/components/ToolsView.vue
+++ client/galaxy/scripts/components/ToolsView.vue
@@ -1,31 +1,85 @@
 <template>
-  <div>
+  <div
+    v-infinite-scroll="loadMore"
+    infinite-scroll-disabled="busy"
+  >
     <h2 class="mb-3">
       <span id="tools-view">Tools View</span>
     </h2>
-    <b-card-group columns>
-      <b-card
-        v-for="(info, index) in toolsExtracted"
-        :key="index"
-        :header="info['section']"
-        :title="info['name']"
-        :sub-title="info['version'] + ' / ' + info['id'] "
+    <div v-if="!loading">
+      <b-container
+        fluid
+        class="mb-4"
       >
-        <p class="card-text">
-          {{ info["description"] }}
-        </p>
-        <Citations
-          simple
-          source="tools"
-          :id="info['id']"
-        />
-      </b-card>
-    </b-card-group>
+        <b-row>
+          <b-col md="6">
+            <b-form-group description="Search for strings or regular expressions">
+              <b-input-group>
+                <b-form-input
+                  v-model="filter"
+                  placeholder="Type to Search"
+                  @keyup.esc.native="filter = ''"
+                />
+                <b-input-group-append>
+                  <b-btn
+                    :disabled="!filter"
+                    @click="filter = ''"
+                  >Clear (esc)</b-btn>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-container>
+      <b-card-group
+        v-for="(deck, dindex) in bufferView"
+        :key="dindex"
+        deck
+        class="mb-4"
+      >
+        <b-card
+          v-for="(info, index) in deck"
+          :key="index"
+        >
+          <div slot="header">
+            <b-link
+              :href="info['url']"
+              target="_blank"
+            >
+              <h4>{{ info['name'] }}</h4>
+            </b-link>
+            <b-badge>
+              {{ info['section'] }}
+            </b-badge>
+          </div>
+          <p class="card-text">
+            {{ info["description"] }}
+          </p>
+          <p class="card-text">
+            <b-btn v-b-modal="'modal-' + dindex + '-' + index">Info</b-btn>
+            <b-modal
+              :id="'modal-' + dindex + '-' + index"
+              centered
+              :title="info['name']"
+            >
+              <b>{{ info['version'] + ' / ' + info['id'] }}</b>
+              <p>{{ info['description'] }}</p>
+            </b-modal>
+          </p>
+          <Citations
+            simple
+            source="tools"
+            :id="info['id']"
+          />
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
 <script>
 import { getAppRoot } from "onload/loadConfig";
+import infiniteScroll from "vue-infinite-scroll";
 import axios from "axios";
 import Citations from "components/Citations.vue";
 
@@ -33,14 +87,37 @@ export default {
     components: {
         Citations
     },
+    directives: { infiniteScroll },
     data() {
         return {
             tools: [],
+            buffer: [],
+            filter: "",
+            busy: false,
             loading: true
         };
     },
     computed: {
-        toolsExtracted() {
+        bufferView() {
+            let a = [];
+            let b = this.bufferFiltered.slice(0);
+            while (b.length) a.push(b.splice(0, 3));
+            return a;
+        },
+        bufferFiltered() {
+            let f = [];
+            let filter = this.filter;
+            if (!this.loading) {
+                f = this.buffer.filter(x => {
+                    let re = new RegExp(filter, "i");
+                    return x["name"].match(re) || x["description"].match(re);
+                });
+            }
+            return f;
+        }
+    },
+    methods: {
+        toolsExtracted(tools) {
             function extractSections(acc, section) {
                 function extractTools(_acc, tool) {
                     return tool["name"]
@@ -59,18 +136,29 @@ export default {
                 }
                 return acc.concat(section["elems"].reduce(extractTools, []));
             }
-            return this.tools
+            return tools
                 .reduce(extractSections, [])
                 .map(a => [Math.random(), a])
                 .sort((a, b) => a[0] - b[0])
                 .map(a => a[1]);
+        },
+        loadMore: function() {
+            this.busy = true;
+
+            setTimeout(() => {
+                const start = this.buffer.length;
+                const end = start + 20;
+                this.buffer = this.buffer.concat(this.tools.slice(start, end));
+                this.busy = false;
+            }, 1000);
         }
     },
     created() {
         axios
             .get(`${getAppRoot()}api/tools`)
             .then(response => {
-                this.tools = response.data;
+                this.tools = this.toolsExtracted(response.data);
+                this.buffer = this.tools.slice(0, 20);
                 this.loading = false;
             })
             .catch(error => {
