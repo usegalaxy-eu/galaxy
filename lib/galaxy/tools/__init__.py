ba0e422eb89acec3a1fb3dd152cbccc9957cf4b1
Sebastian Kapust <sebastian@kapust.org>
2019-06-02 22:24:47 +0200

    Include help text summary in tools view cards

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -1916,6 +1916,12 @@ class Tool(Dictifiable):
         tool_class = self.__class__
         regular_form = tool_class == Tool or isinstance(self, DatabaseOperationTool)
         tool_dict["form_style"] = "regular" if regular_form else "special"
+        # create tool help
+        tool_help = ''
+        if self.help:
+            tool_help = self.help.render(static_path=self.app.url_for('/static'), host_url=self.app.url_for('/', qualified=True))
+            tool_help = unicodify(tool_help, 'utf-8')
+        tool_dict["help"] = tool_help
 
         return tool_dict
 
