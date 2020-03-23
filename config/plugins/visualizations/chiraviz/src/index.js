fb1a8f88607adcf9d32fcb3f3e335f96e00d5066
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-03-23 17:56:30 +0100

    intoroduce webpack

---
+++ config/plugins/visualizations/chiraviz/src/index.js
@@ -0,0 +1,14 @@
+import './css/rna.viz.css'
+import 'script-loader!jquery'
+import 'script-loader!./js/rna-viz'
+import 'script-loader!underscore/underscore-min'
+import 'script-loader!plotly.js-dist'
+
+import './js/visualize-alignment'
+
+require('bootstrap');
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+
+require('font-awesome/css/font-awesome.css');
