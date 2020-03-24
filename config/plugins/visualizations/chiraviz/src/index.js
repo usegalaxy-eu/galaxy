dca4ee7a54995455c800d20cf6d6b58afa50cb32
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-03-24 17:41:27 +0100

    fix css issues, sync dependecies versions

--- config/plugins/visualizations/chiraviz/src/index.js
+++ config/plugins/visualizations/chiraviz/src/index.js
@@ -1,14 +1,9 @@
-import './css/rna.viz.css'
 import 'script-loader!jquery'
 import 'script-loader!./js/rna-viz'
 import 'script-loader!underscore/underscore-min'
-import 'script-loader!plotly.js-dist'
-
+import 'script-loader!plotly.js/dist/plotly.min'
 import 'script-loader!./js/visualize-alignment'
-
-require('bootstrap');
-
+import 'script-loader!bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
-
-require('font-awesome/css/font-awesome.css');
+import 'font-awesome/css/font-awesome.min.css';
+import './css/rna.viz.css'
