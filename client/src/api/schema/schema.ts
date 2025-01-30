5625513439b1cc0cdb4135161b06cee888120a41
José Manuel Domínguez <dominguj@informatik.uni-freiburg.de>
2025-01-30 09:44:31 +0100

    UPSTREAM 25.0: eLabFTW file source from file source templates
    Support user-defined file sources based on `eLabFTWFilesSource`. Add a file source template for eLabFTW and the required documentation.
    
    Set `writable: true` in file source template for eLabFTW
    
    Make `writable` an option
    
    Let users choose whether Galaxy should be allowed to export data to eLabFTW or they only need to import data to Galaxy.

--- client/src/api/schema/schema.ts
+++ client/src/api/schema/schema.ts
@@ -9250,7 +9250,7 @@ export interface components {
              * Type
              * @enum {string}
              */
-            type: "ftp" | "posix" | "s3fs" | "azure" | "onedata" | "webdav" | "dropbox" | "googledrive";
+            type: "ftp" | "posix" | "s3fs" | "azure" | "onedata" | "webdav" | "dropbox" | "googledrive" | "elabftw";
             /** Variables */
             variables?:
                 | (
@@ -17627,7 +17627,7 @@ export interface components {
              * Type
              * @enum {string}
              */
-            type: "ftp" | "posix" | "s3fs" | "azure" | "onedata" | "webdav" | "dropbox" | "googledrive";
+            type: "ftp" | "posix" | "s3fs" | "azure" | "onedata" | "webdav" | "dropbox" | "googledrive" | "elabftw";
             /** Uri Root */
             uri_root: string;
             /**
