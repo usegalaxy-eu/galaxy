d51b8e5153c9f012efbcf730ea828e1fd6299d60
Julian Weidhase <weidhase.jw@gmail.com>
2026-06-22 15:51:03 +0200

    Add ARC file source support

--- lib/galaxy/files/templates/models.py
+++ lib/galaxy/files/templates/models.py
@@ -36,6 +36,7 @@ FileSourceTemplateType = Literal[
     "ftp",
     "posix",
     "s3fs",
+    "arc",
     "azure",
     "azureflat",
     "irods",
@@ -293,6 +294,22 @@ class OnedataFileSourceConfiguration(StrictModel):
     writable: bool = False
 
 
+class ArcFileSourceTemplateConfiguration(StrictModel):
+    type: Literal["arc"]
+    base_url: Union[str, TemplateExpansion]
+    token: Union[str, TemplateExpansion, None] = None
+    writable: Union[bool, TemplateExpansion] = False
+    template_start: Optional[str] = None
+    template_end: Optional[str] = None
+
+
+class ArcFileSourceConfiguration(StrictModel):
+    type: Literal["arc"]
+    base_url: str
+    token: Optional[str] = None
+    writable: bool = False
+
+
 class WebdavConfigMixin:
     @model_validator(mode="before")
     @classmethod
@@ -467,6 +484,7 @@ FileSourceTemplateConfiguration = Annotated[
         AzureFlatFileSourceTemplateConfiguration,
         IrodsFileSourceTemplateConfiguration,
         OnedataFileSourceTemplateConfiguration,
+        ArcFileSourceTemplateConfiguration,
         WebdavFileSourceTemplateConfiguration,
         DropboxFileSourceTemplateConfiguration,
         GoogleDriveFileSourceTemplateConfiguration,
@@ -493,6 +511,7 @@ FileSourceConfiguration = Annotated[
         AzureFlatFileSourceConfiguration,
         IrodsFileSourceConfiguration,
         OnedataFileSourceConfiguration,
+        ArcFileSourceConfiguration,
         WebdavFileSourceConfiguration,
         DropboxFileSourceConfiguration,
         GoogleDriveFileSourceConfiguration,
@@ -577,6 +596,7 @@ TypesToConfigurationClasses: dict[FileSourceTemplateType, type[FileSourceConfigu
     "azureflat": AzureFlatFileSourceConfiguration,
     "irods": IrodsFileSourceConfiguration,
     "onedata": OnedataFileSourceConfiguration,
+    "arc": ArcFileSourceConfiguration,
     "webdav": WebdavFileSourceConfiguration,
     "dropbox": DropboxFileSourceConfiguration,
     "googledrive": GoogleDriveFileSourceConfiguration,
